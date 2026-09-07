4d9354da5d295d1d6dd9f1e3a4ac092450ed8d66
Muhammad Ridwan <157098120+Muhd-Ridwan@users.noreply.github.com>
2026-09-07 17:03:39 +0800

    Fix type

--- Digital-Scholarship-Management-System/ClientApp/src/environments/environment.prod.ts
+++ Digital-Scholarship-Management-System/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'apiUrl: https://sobc0bt9ri.execute-api.ap-southeast-2.amazonaws.com/prod/api',
+  apiUrl: 'https://sobc0bt9ri.execute-api.ap-southeast-2.amazonaws.com/prod/api',
   cognito: {
     userPoolId: 'ap-southeast-2_dSx48w01Z',
     userPoolClientId: '1m7vgn24n7b7i9spv88tvg63vk',
