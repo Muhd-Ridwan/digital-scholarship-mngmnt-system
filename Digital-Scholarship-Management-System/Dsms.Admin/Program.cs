3b064f36fa2bcd7f5dc0cbe90debe7843f29ce72
Muhammad Ridwan <157098120+Muhd-Ridwan@users.noreply.github.com>
2026-09-17 23:03:32 +0800

    (X-Ray) Adding traces for admin

--- Digital-Scholarship-Management-System/Dsms.Admin/Program.cs
+++ Digital-Scholarship-Management-System/Dsms.Admin/Program.cs
@@ -10,8 +10,12 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text.Json.Serialization;
+using Amazon.XRay.Recorder.Handlers.AwsSdk;
+using Amazon.XRay.Recorder.Handlers.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+AWSSDKHandler.RegisterXRayForAllServices();
 
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
@@ -88,6 +92,8 @@ builder.Services.AddCors(options =>
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
 
 var app = builder.Build();
+
+app.UseXRay("dsms-admin-service");
 
 app.UseCors("AllowFrontend");
 app.UseAuthentication();
